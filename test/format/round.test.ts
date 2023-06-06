be286f39022a966334734ac06d5fd27c759be6e4
Nael Shiab <nael.shiab@gmail.com>
2023-06-06 09:14:43 -0400

    round done

---
+++ test/format/round.test.ts
@@ -0,0 +1,37 @@
+import assert from "assert"
+import round from "../../src/format/round.js"
+
+describe("round", () => {
+    it("should keep an integer as integer", () => {
+        const rounded = round(1)
+        assert.strictEqual(rounded, 1)
+    })
+    it("should round 1.2 to 1", () => {
+        const rounded = round(1.2)
+        assert.strictEqual(rounded, 1)
+    })
+    it("should round 1.5 to 2", () => {
+        const rounded = round(1.5)
+        assert.strictEqual(rounded, 2)
+    })
+    it("should round 1.8 to 2", () => {
+        const rounded = round(1.8)
+        assert.strictEqual(rounded, 2)
+    })
+    it("should round 1.1234567 to 1.1", () => {
+        const rounded = round(1.1234567, { nbDecimals: 1 })
+        assert.strictEqual(rounded, 1.1)
+    })
+    it("should round 1.1234567 to 1.12346", () => {
+        const rounded = round(1.1234567, { nbDecimals: 5 })
+        assert.strictEqual(rounded, 1.12346)
+    })
+    it("should round 12345 to the closest 10", () => {
+        const rounded = round(12345, { nearestInteger: 10 })
+        assert.strictEqual(rounded, 12350)
+    })
+    it("should round 12345 to the closest 100", () => {
+        const rounded = round(12345, { nearestInteger: 100 })
+        assert.strictEqual(rounded, 12300)
+    })
+})
