2fc00d426b72f2d6d64bdf57b701118f6b457140
Adam Nyx <adam.nyx@cbc.ca>
2023-06-21 14:50:00 -0400

    format, add test

--- test/weather/getHumidex.test.ts
+++ test/weather/getHumidex.test.ts
@@ -1,9 +1,12 @@
 import assert from "assert"
-import getHumidex from "../../src/weather/getHumidex"
+import getHumidex from "../../src/weather/getHumidex.js"
 
 describe("getHumidex", () => {
     it("should return the humidex given temperature and humidity", () => {
-        const humidex = getHumidex(30, 86)
+        const humidex = getHumidex(30, 70)
         assert.equal(humidex, 41)
+    })
+    it("should throw error when humidex is not between 0 and 100", () => {
+        assert.throws(() => getHumidex(30, 105))
     })
 })
