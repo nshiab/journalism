c7b77eb5a2d9b5a7371f187fdb8387b7f91c67e5
Nael Shiab <nael.shiab@gmail.com>
2023-12-21 09:35:00 -0500

    Updating modules, adding esm bundle.

---
+++ src/bundle.ts
@@ -0,0 +1,27 @@
+import formatDate from "./format/formatDate.js"
+import formatDateLocal from "./format/formatDateLocal.js"
+import formatNumber from "./format/formatNumber.js"
+import round from "./format/round.js"
+import geoTo3D from "./geo/geoTo3D.js"
+import distance from "./geo/distance.js"
+import adjustToInflation from "./finance/adjustToInflation.js"
+import mortgagePayments from "./finance/mortgagePayments.js"
+import mortgageInsurancePremium from "./finance/mortgageInsurancePremium.js"
+import mortgageMaxAmount from "./finance/mortgageMaxAmount.js"
+import getClosest from "./geo/getClosest.js"
+import getHumidex from "./weather/getHumidex.js"
+
+export {
+    formatDate,
+    formatDateLocal,
+    formatNumber,
+    round,
+    geoTo3D,
+    distance,
+    adjustToInflation,
+    mortgagePayments,
+    mortgageInsurancePremium,
+    mortgageMaxAmount,
+    getClosest,
+    getHumidex,
+}
