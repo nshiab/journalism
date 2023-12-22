d0b645833ffdea947173f6e1ad5a09aa859f6692
Nael Shiab <nael.shiab@gmail.com>
2023-12-22 10:57:40 -0500

    adding savePlotChart

--- src/dataviz/savePlotChart.ts
+++ src/dataviz/savePlotChart.ts
@@ -2,14 +2,30 @@ import puppeteer from "puppeteer"
 import { Plot } from "@observablehq/plot"
 
 export default async function savePlotChart(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    data: { [key: string]: any },
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    makeChart: (data: any) => (SVGSVGElement | HTMLElement) & Plot,
+    data: { [key: string]: unknown }[],
+    makeChart: (
+        data: { [key: string]: unknown }[]
+    ) => (SVGSVGElement | HTMLElement) & Plot,
     path: string
 ) {
+    // We check which keys hold dates, based on the first data item.
+    const keysWithDates = Object.keys(data[0]).filter(
+        (key) => data[0][key] instanceof Date
+    )
+    // And we convert the dates to ms in the data. They will be easier to convert back to dates this way.
+    if (keysWithDates.length > 0) {
+        for (const d of data) {
+            for (const key of keysWithDates) {
+                d[key] = (d[key] as Date).getTime()
+            }
+        }
+    }
+
     const browser = await puppeteer.launch({ headless: "new" })
     const page = await browser.newPage()
+    // For better screenshot resolution
+    page.setViewport({ width: 1000, height: 1000, deviceScaleFactor: 2 })
+
     // We inject the d3 and plot code.
     await page.addScriptTag({
         path: "node_modules/d3/dist/d3.js",
@@ -17,11 +33,19 @@ export default async function savePlotChart(
     await page.addScriptTag({
         path: "node_modules/@observablehq/plot/dist/plot.umd.js",
     })
+
     // We create an empty div.
-    await page.setContent("<div id='dataviz'></div>")
-    // We generate the chart and append it to our div.
+    await page.setContent("<div id='dataviz' style='display:flex;'></div>")
+
+    // We convert back dates, generate the chart and append it to our div.
     await page.evaluate(`
         const data = ${JSON.stringify(data)}
+        const keysWithDates = ${JSON.stringify(keysWithDates)}
+        for (const key of keysWithDates) {
+            for (const d of data) {
+                d[key] = new Date(d[key])
+            }
+        }
         const makeChart = ${makeChart.toString()}
         const plot = makeChart(data)
         const div = document.querySelector("#dataviz")
@@ -29,8 +53,9 @@ export default async function savePlotChart(
             throw new Error("No div with id dataviz")
         }
         div.append(plot)`)
-    // We select the div and save a screenshot of it.
-    const dataviz = await page.$("#dataviz")
+
+    // We select the generated figure or svg and save a screenshot of it.
+    const dataviz = await page.$("#dataviz > figure, #dataviz > svg")
     if (!dataviz) {
         throw new Error("No dataviz element.")
     }
--- test/dataviz/savePlotChart.test.ts
+++ test/dataviz/savePlotChart.test.ts
@@ -4,36 +4,27 @@ import * as Plot from "@observablehq/plot"
 describe("savePlotChart", () => {
     it("save a chart", async () => {
         const data = [
-            { age: 35, salary: 75000, department: "Engineering" },
-            { age: 42, salary: 82000, department: "Sales" },
-            { age: 28, salary: 60000, department: "Marketing" },
-            { age: 55, salary: 90000, department: "HR" },
-            { age: 49, salary: 72000, department: "Finance" },
-            { age: 31, salary: 55000, department: "Engineering" },
-            { age: 40, salary: 68000, department: "Sales" },
-            { age: 23, salary: 48000, department: "HR" },
-            { age: 37, salary: 77000, department: "Marketing" },
-            { age: 45, salary: 88000, department: "Engineering" },
-            { age: 33, salary: 71000, department: "Finance" },
-            { age: 50, salary: 82000, department: "HR" },
-            { age: 26, salary: 63000, department: "Sales" },
-            { age: 41, salary: 76000, department: "Marketing" },
-            { age: 29, salary: 59000, department: "Engineering" },
-            { age: 38, salary: 79000, department: "Finance" },
-            { age: 47, salary: 85000, department: "HR" },
-            { age: 32, salary: 64000, department: "Sales" },
-            { age: 44, salary: 83000, department: "Marketing" },
-            { age: 30, salary: 60000, department: "Engineering" },
+            { salary: 75000, hireDate: new Date("2022-12-15") },
+            { salary: 82000, hireDate: new Date("2022-11-20") },
+            { salary: 60000, hireDate: new Date("2022-10-25") },
+            { salary: 90000, hireDate: new Date("2022-09-30") },
+            { salary: 72000, hireDate: new Date("2022-08-15") },
+            { salary: 55000, hireDate: new Date("2022-07-20") },
+            { salary: 68000, hireDate: new Date("2022-06-25") },
+            { salary: 48000, hireDate: new Date("2022-05-30") },
+            { salary: 77000, hireDate: new Date("2022-04-15") },
+            { salary: 88000, hireDate: new Date("2022-03-20") },
         ]
 
         await savePlotChart(
             data,
             (data) =>
                 Plot.plot({
+                    marginLeft: 75,
                     color: { legend: true },
                     marks: [
                         Plot.dot(data, {
-                            x: "age",
+                            x: "hireDate",
                             y: "salary",
                             stroke: "salary",
                         }),
