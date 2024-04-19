eaa87d0e9e77e1e0e86bc923adf7d72f10e94e76
Nael Shiab <nael.shiab@gmail.com>
2024-04-19 14:41:32 -0400

    Documentation

--- src/other/unzip.ts
+++ src/other/unzip.ts
@@ -1,5 +1,13 @@
 import AdmZip from "adm-zip"
 
+/**
+ * Unzips a file and output the result in a folder.
+ *
+ * ```js
+ * unzip("files.zip", "./output/")
+ * ```
+ * @category Other
+ */
 export default function unzip(zippedFile: string, output: string) {
     const zip = new AdmZip(zippedFile)
     zip.extractAllTo(output, true)
