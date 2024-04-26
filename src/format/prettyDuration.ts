289b80a1fd0bdf9df1f2b4449ea9aca307549632
Nael Shiab <nael.shiab@gmail.com>
2024-04-26 09:06:04 -0400

    push

--- src/format/prettyDuration.ts
+++ src/format/prettyDuration.ts
@@ -1,42 +1,69 @@
 /**
- * Returns the duration between two dates as a string in terms of milliseconds, seconds, minutes, hours, and days.
+ * Returns the duration as a string in terms of milliseconds, seconds, minutes, hours, and days.
+ *
  * ```js
- * const duration = prettyDuration(
- *   new Date("2024-01-01T17:00:00.000"),
- *   new Date("2024-01-23T23:03:15.003")
- * )
- * // Returns "22 days, 6 h, 3 min, 15 sec, 3 ms"
+ * // A starting Date somewhere in your code
+ * const startDate = new Date() // or Date.now()
+ *
+ * // When you want to know the elapsed duration, pass the start date
+ * const duration = prettyDuration(startDate)
+ * // Returns something like "22 days, 6 h, 3 min, 15 sec, 3 ms"
+ *
+ * // If you want to console.log it, set the option log to true
+ * prettyDuration(startDate, {log: true})
+ *
+ * // You can also use a prefix and/or suffix
+ * prettyDuration(startDate, {log: true, prefix: "Total duration: ", suffix: " (Main function)"})
+ * // Returns and logs something like "Total duration: 3 min, 15 sec, 3 ms (Main function)"
+ *
+ * // If you want to format the duration between two specific dates, use the end option.
+ * prettyDuration(new Date("2024-01-01T17:00:00"), { end: new Date("2024-01-23T23:03:15") })
+ * // Returns "22 days, 6 h, 3 min, 15 sec, 0 ms"
  * ```
+ *
  * @category Formatting
  */
 export default function prettyDuration(
     start: Date | number,
-    end: Date | number
+    options: {
+        log?: boolean
+        end?: Date | number
+        prefix?: string
+        suffix?: string
+    } = {}
 ): string {
     if (start instanceof Date) {
         start = start.getTime()
     }
-    if (end instanceof Date) {
-        end = end.getTime()
+
+    let end
+    if (options.end instanceof Date) {
+        end = options.end.getTime()
+    } else if (typeof options.end === "number") {
+        end = options.end
+    } else {
+        end = Date.now()
     }
 
     const differenceInMs = end - start
 
+    let prettyDuration = ""
+
     if (differenceInMs < 1000) {
         // Less than a second
-        return `${differenceInMs} ms`
+        prettyDuration = `${differenceInMs} ms`
     } else if (differenceInMs < 60_000) {
         // Less than a minute
         const sec = Math.floor(differenceInMs / 1000)
         const ms = differenceInMs % 1000
-        return `${sec} sec, ${ms} ms`
+        prettyDuration = `${sec} sec, ${ms} ms`
     } else if (differenceInMs < 3_600_000) {
         // Less than an hour
         const min = Math.floor(differenceInMs / 60_000)
         const remainingMs = differenceInMs % 60_000
         const sec = Math.floor(remainingMs / 1000)
         const ms = remainingMs % 1000
-        return `${min} min, ${sec} sec, ${ms} ms`
+        prettyDuration = `${min} min, ${sec} sec, ${ms} ms`
     } else if (differenceInMs < 86_400_000) {
         // Less than a day
         const hours = Math.floor(differenceInMs / 3_600_000)
@@ -45,7 +72,7 @@ export default function prettyDuration(
         const remainingMsAfterMinutes = remainingMsAfterHours % 60_000
         const sec = Math.floor(remainingMsAfterMinutes / 1000)
         const ms = remainingMsAfterMinutes % 1000
-        return `${hours} h, ${min} min, ${sec} sec, ${ms} ms`
+        prettyDuration = `${hours} h, ${min} min, ${sec} sec, ${ms} ms`
     } else {
         // At least one day
         const days = Math.floor(differenceInMs / 86_400_000)
@@ -56,6 +83,19 @@ export default function prettyDuration(
         const remainingMsAfterMin = remainingMsAfterHours % 60_000
         const sec = Math.floor(remainingMsAfterMin / 1000)
         const ms = remainingMsAfterMin % 1000
-        return `${days} days, ${hours} h, ${min} min, ${sec} sec, ${ms} ms`
+        prettyDuration = `${days} days, ${hours} h, ${min} min, ${sec} sec, ${ms} ms`
     }
+
+    if (typeof options.prefix === "string") {
+        prettyDuration = `${options.prefix}${prettyDuration}`
+    }
+    if (typeof options.suffix === "string") {
+        prettyDuration = `${prettyDuration}${options.suffix}`
+    }
+
+    if (options.log === true) {
+        console.log(prettyDuration)
+    }
+
+    return prettyDuration
 }
--- test/format/prettyDuration.test.ts
+++ test/format/prettyDuration.test.ts
@@ -3,38 +3,62 @@ import prettyDuration from "../../src/format/prettyDuration.js"
 
 describe("prettyDuration", () => {
     it("should return a string with a number of ms", () => {
-        const duration = prettyDuration(
-            new Date("2024-01-01T17:00:00"),
-            new Date("2024-01-01T17:00:00.015")
-        )
+        const duration = prettyDuration(new Date("2024-01-01T17:00:00"), {
+            end: new Date("2024-01-01T17:00:00.015"),
+            log: true,
+        })
         assert.strictEqual(duration, "15 ms")
     })
     it("should return a string with a number of seconds", () => {
-        const duration = prettyDuration(
-            new Date("2024-01-01T17:00:00"),
-            new Date("2024-01-01T17:00:15")
-        )
+        const duration = prettyDuration(new Date("2024-01-01T17:00:00"), {
+            end: new Date("2024-01-01T17:00:15"),
+        })
         assert.strictEqual(duration, "15 sec, 0 ms")
     })
     it("should return a string with a number of seconds and minutes", () => {
-        const duration = prettyDuration(
-            new Date("2024-01-01T17:00:00"),
-            new Date("2024-01-01T17:03:15")
-        )
+        const duration = prettyDuration(new Date("2024-01-01T17:00:00"), {
+            end: new Date("2024-01-01T17:03:15"),
+        })
         assert.strictEqual(duration, "3 min, 15 sec, 0 ms")
     })
     it("should return a string with a number of seconds, minutes, and hours", () => {
-        const duration = prettyDuration(
-            new Date("2024-01-01T17:00:00"),
-            new Date("2024-01-01T23:03:15")
-        )
+        const duration = prettyDuration(new Date("2024-01-01T17:00:00"), {
+            end: new Date("2024-01-01T23:03:15"),
+        })
         assert.strictEqual(duration, "6 h, 3 min, 15 sec, 0 ms")
     })
     it("should return a string with a number of seconds, minutes, hours, and days", () => {
-        const duration = prettyDuration(
-            new Date("2024-01-01T17:00:00"),
-            new Date("2024-01-23T23:03:15")
-        )
+        const duration = prettyDuration(new Date("2024-01-01T17:00:00"), {
+            end: new Date("2024-01-23T23:03:15"),
+        })
         assert.strictEqual(duration, "22 days, 6 h, 3 min, 15 sec, 0 ms")
+    })
+    it("should return a string with a number of seconds, minutes, hours, and days. It should also add a prefix.", () => {
+        const duration = prettyDuration(new Date("2024-01-01T17:00:00"), {
+            end: new Date("2024-01-23T23:03:15"),
+            prefix: "Total duration: ",
+        })
+        assert.strictEqual(
+            duration,
+            "Total duration: 22 days, 6 h, 3 min, 15 sec, 0 ms"
+        )
+    })
+    it("should return a string with a number of seconds, minutes, hours, and days. It should also add a suffix.", () => {
+        const duration = prettyDuration(new Date("2024-01-01T17:00:00"), {
+            end: new Date("2024-01-23T23:03:15"),
+            suffix: " (test)",
+        })
+        assert.strictEqual(duration, "22 days, 6 h, 3 min, 15 sec, 0 ms (test)")
+    })
+    it("should return a string with a number of seconds, minutes, hours, and days. It should also add a prefix and a suffix.", () => {
+        const duration = prettyDuration(new Date("2024-01-01T17:00:00"), {
+            end: new Date("2024-01-23T23:03:15"),
+            prefix: "Total duration: ",
+            suffix: " (test)",
+        })
+        assert.strictEqual(
+            duration,
+            "Total duration: 22 days, 6 h, 3 min, 15 sec, 0 ms (test)"
+        )
     })
 })
