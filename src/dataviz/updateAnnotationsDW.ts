c67af33d86cbf4a3ecdccae7040e18d7ae1197d0
Graeme Bruce <graeme.bruce@cbc.ca>
2024-04-29 08:51:44 -0400

    cleanup

--- src/dataviz/updateAnnotationsDW.ts
+++ src/dataviz/updateAnnotationsDW.ts
@@ -9,12 +9,12 @@
  * const myAnnotations = [
     {
     "x": "2024/08/30 01:52",
-    "y": "14496235.1500",
+    "y": "14496235",
     "text": "This is an annotation!",
     },
     {
     "x": "2024/06/29",
-        "y": "15035128.6500",
+        "y": "15035128",
         "dy": "50",
         "text": "This is also some text, but with an arrow!",
         "connectorLine": {
