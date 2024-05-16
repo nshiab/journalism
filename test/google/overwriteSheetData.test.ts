f5e11820a251df0c08169c697acc51b9f7fe64ab
Nael Shiab <nael.shiab@gmail.com>
2024-05-16 15:00:22 -0400

    updating tests and workflows

--- test/google/overwriteSheetData.test.ts
+++ test/google/overwriteSheetData.test.ts
@@ -13,76 +13,83 @@ const data = [
 // Commented tests because too many requests on API.
 
 describe("overwriteSheetData", () => {
-    // it("should overwrite the data in a sheet", async () => {
-    //     await overwriteSheetData(data, sheetUrl)
+    const email = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL
+    const key = process.env.GOOGLE_PRIVATE_KEY
 
-    //     const csv = await getSheetData(sheetUrl, { csv: true })
+    if (typeof email === "string" && typeof key === "string") {
+        // it("should overwrite the data in a sheet", async () => {
+        //     await overwriteSheetData(data, sheetUrl)
 
-    //     assert.deepStrictEqual(csv, "first,last\r\nNael,Shiab\r\nAndrew,Ryan")
-    // })
-    // it("should overwrite the data in a sheet as raw values", async () => {
-    //     await overwriteSheetData(data, sheetUrl, { raw: true })
+        //     const csv = await getSheetData(sheetUrl, { csv: true })
 
-    //     const csv = await getSheetData(sheetUrl, { csv: true })
+        //     assert.deepStrictEqual(csv, "first,last\r\nNael,Shiab\r\nAndrew,Ryan")
+        // })
+        // it("should overwrite the data in a sheet as raw values", async () => {
+        //     await overwriteSheetData(data, sheetUrl, { raw: true })
 
-    //     assert.deepStrictEqual(csv, "first,last\r\nNael,Shiab\r\nAndrew,Ryan")
-    // })
-    // it("should overwrite the data in a sheet with a lastUpdate option", async () => {
-    //     await overwriteSheetData(data, sheetUrl, { lastUpdate: true })
+        //     const csv = await getSheetData(sheetUrl, { csv: true })
 
-    //     const csv = await getSheetData(sheetUrl, { csv: true })
+        //     assert.deepStrictEqual(csv, "first,last\r\nNael,Shiab\r\nAndrew,Ryan")
+        // })
+        // it("should overwrite the data in a sheet with a lastUpdate option", async () => {
+        //     await overwriteSheetData(data, sheetUrl, { lastUpdate: true })
 
-    //     assert.deepStrictEqual(
-    //         (csv as string).replace(
-    //             /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/,
-    //             "DATE"
-    //         ),
-    //         "Last update:,DATE UTC\r\nfirst,last\r\nNael,Shiab\r\nAndrew,Ryan"
-    //     )
-    // })
-    // it("should overwrite the data in a sheet with a lastUpdate option and a specific time zone", async () => {
-    //     await overwriteSheetData(data, sheetUrl, {
-    //         lastUpdate: true,
-    //         timeZone: "Canada/Eastern",
-    //     })
+        //     const csv = await getSheetData(sheetUrl, { csv: true })
 
-    //     const csv = await getSheetData(sheetUrl, { csv: true })
+        //     assert.deepStrictEqual(
+        //         (csv as string).replace(
+        //             /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/,
+        //             "DATE"
+        //         ),
+        //         "Last update:,DATE UTC\r\nfirst,last\r\nNael,Shiab\r\nAndrew,Ryan"
+        //     )
+        // })
+        // it("should overwrite the data in a sheet with a lastUpdate option and a specific time zone", async () => {
+        //     await overwriteSheetData(data, sheetUrl, {
+        //         lastUpdate: true,
+        //         timeZone: "Canada/Eastern",
+        //     })
 
-    //     assert.deepStrictEqual(
-    //         (csv as string).replace(
-    //             /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/,
-    //             "DATE"
-    //         ),
-    //         "Last update:,DATE ET\r\nfirst,last\r\nNael,Shiab\r\nAndrew,Ryan"
-    //     )
-    // })
-    // it("should overwrite the data in a sheet with a prepended text", async () => {
-    //     await overwriteSheetData(data, sheetUrl, {
-    //         prepend: "Contact me for more info",
-    //     })
+        //     const csv = await getSheetData(sheetUrl, { csv: true })
 
-    //     const csv = await getSheetData(sheetUrl, { csv: true })
+        //     assert.deepStrictEqual(
+        //         (csv as string).replace(
+        //             /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/,
+        //             "DATE"
+        //         ),
+        //         "Last update:,DATE ET\r\nfirst,last\r\nNael,Shiab\r\nAndrew,Ryan"
+        //     )
+        // })
+        // it("should overwrite the data in a sheet with a prepended text", async () => {
+        //     await overwriteSheetData(data, sheetUrl, {
+        //         prepend: "Contact me for more info",
+        //     })
 
-    //     assert.deepStrictEqual(
-    //         csv,
-    //         "Contact me for more info,\r\nfirst,last\r\nNael,Shiab\r\nAndrew,Ryan"
-    //     )
-    // })
-    it("should overwrite the data in a sheet with a prepended text and lastUpdate with a specific time zone", async () => {
-        await overwriteSheetData(data, sheetUrl, {
-            prepend: "Contact me for more info",
-            lastUpdate: true,
-            timeZone: "Canada/Eastern",
+        //     const csv = await getSheetData(sheetUrl, { csv: true })
+
+        //     assert.deepStrictEqual(
+        //         csv,
+        //         "Contact me for more info,\r\nfirst,last\r\nNael,Shiab\r\nAndrew,Ryan"
+        //     )
+        // })
+        it("should overwrite the data in a sheet with a prepended text and lastUpdate with a specific time zone", async () => {
+            await overwriteSheetData(data, sheetUrl, {
+                prepend: "Contact me for more info",
+                lastUpdate: true,
+                timeZone: "Canada/Eastern",
+            })
+
+            const csv = await getSheetData(sheetUrl, { csv: true })
+
+            assert.deepStrictEqual(
+                (csv as string).replace(
+                    /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/,
+                    "DATE"
+                ),
+                "Contact me for more info,\r\nLast update:,DATE ET\r\nfirst,last\r\nNael,Shiab\r\nAndrew,Ryan"
+            )
         })
-
-        const csv = await getSheetData(sheetUrl, { csv: true })
-
-        assert.deepStrictEqual(
-            (csv as string).replace(
-                /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/,
-                "DATE"
-            ),
-            "Contact me for more info,\r\nLast update:,DATE ET\r\nfirst,last\r\nNael,Shiab\r\nAndrew,Ryan"
-        )
-    })
+    } else {
+        console.log("No Google service account email or no google private key.")
+    }
 })
