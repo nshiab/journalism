bb13658e8c5d48872aa4450855488a59bf5e31d6
Nael Shiab <nael.shiab@gmail.com>
2024-07-02 15:01:04 -0400

    Adding an option to remove zipped file after extraction

--- src/other/unzip.ts
+++ src/other/unzip.ts
@@ -1,4 +1,5 @@
 import AdmZip from "adm-zip"
+import { unlinkSync } from "fs"
 
 /**
  * Unzips a file and outputs the result in a folder.
@@ -6,9 +7,22 @@ import AdmZip from "adm-zip"
  * ```js
  * unzip("files.zip", "./output/")
  * ```
+ *
+ * There is an option to remove the zipped file after extraction.
+ * ```js
+ * unzip("files.zip", "./output/", { deleteZippedFile: true })
+ * ```
+ *
  * @category Other
  */
-export default function unzip(zippedFile: string, output: string) {
+export default function unzip(
+    zippedFile: string,
+    output: string,
+    options: { deleteZippedFile?: boolean } = {}
+) {
     const zip = new AdmZip(zippedFile)
     zip.extractAllTo(output, true)
+    if (options.deleteZippedFile) {
+        unlinkSync(zippedFile)
+    }
 }
--- test/other/unzip.test.ts
+++ test/other/unzip.test.ts
@@ -1,6 +1,6 @@
 // import assert from "assert"
 import unzip from "../../src/other/unzip.js"
-import { existsSync, mkdirSync } from "fs"
+import { existsSync, mkdirSync, copyFileSync } from "fs"
 
 const outputPath = "./test/output/"
 if (!existsSync(outputPath)) {
@@ -10,6 +10,13 @@ if (!existsSync(outputPath)) {
 describe("unzip", () => {
     it("should unzip and put files in a folder", () => {
         unzip("test/data/test.zip", "test/output")
+        // How to assert?
+    })
+    it("should unzip, put files in a folder and remove original file", () => {
+        copyFileSync("test/data/test.zip", "test/data/testCopy.zip")
+        unzip("test/data/testCopy.zip", "test/output", {
+            deleteZippedFile: true,
+        })
         // How to assert?
     })
 })
