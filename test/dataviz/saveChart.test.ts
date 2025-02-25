b224878c25dfe59a121a4c7262b14938f0f1e2c1
Nael Shiab <nael.shiab@gmail.com>
2025-02-25 15:33:17 -0500

    New options style and dark for saveChart

--- test/dataviz/saveChart.test.ts
+++ test/dataviz/saveChart.test.ts
@@ -13,9 +13,86 @@ Deno.test("should save an Observable chart as png", async () => {
   await saveChart(data, (data) =>
     plot({
       title: "Temperature in cities",
+      subtitle: "Daily temperatures in 2000",
+      caption: "Source: Environment Canada",
       color: { legend: true },
       marks: [line(data, { x: "time", y: "t", stroke: "city" })],
     }), `test/output/temperatures.png`);
+
+  // How to assert
+  assertEquals(true, true);
+});
+Deno.test("should save an Observable chart as png with style options", async () => {
+  const data = JSON.parse(
+    readFileSync("test/data/temperatures.json", "utf-8"),
+  ).map((d: { time: string }) => ({ ...d, time: new Date(d.time) }));
+
+  await saveChart(
+    data,
+    (data) =>
+      plot({
+        title: "Temperature in cities",
+        subtitle: "Daily temperatures in 2000",
+        caption: "Source: Environment Canada",
+        color: { legend: true },
+        x: { label: "Date" },
+        grid: true,
+        marks: [line(data, { x: "time", y: "t", stroke: "city" })],
+      }),
+    `test/output/temperatures-styled.png`,
+    {
+      style: `
+  body {
+    background-color: #121212; /* Very dark grey for the background */
+    color: #E0E0E0; /* Soft light grey for text */
+  }
+
+  h2, h3 {
+    color: #F0F0F0; /* Slightly brighter grey for headings */
+  }
+
+  svg text {
+    fill: #B0B0B0; /* Medium grey for text within the SVG */
+  }
+
+  g[aria-label="y-axis tick"],
+  g[aria-label="x-axis tick"] {
+    stroke: #707070; /* Neutral grey for axis ticks */
+  }
+
+  g[aria-label="x-grid"] > line,
+  g[aria-label="y-grid"] > line {
+    stroke: #505050; /* Subtle grey for grid lines */
+    stroke-opacity: 0.3;
+  }`,
+    },
+  );
+
+  // How to assert
+  assertEquals(true, true);
+});
+Deno.test("should save an Observable chart as png with dark style", async () => {
+  const data = JSON.parse(
+    readFileSync("test/data/temperatures.json", "utf-8"),
+  ).map((d: { time: string }) => ({ ...d, time: new Date(d.time) }));
+
+  await saveChart(
+    data,
+    (data) =>
+      plot({
+        title: "Temperature in cities",
+        subtitle: "Daily temperatures in 2000",
+        caption: "Source: Environment Canada",
+        color: { legend: true },
+        x: { label: "Date" },
+        grid: true,
+        marks: [line(data, { x: "time", y: "t", stroke: "city" })],
+      }),
+    `test/output/temperatures-dark.png`,
+    {
+      dark: true,
+    },
+  );
 
   // How to assert
   assertEquals(true, true);
