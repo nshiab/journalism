081e6c9b98294c0eccd40cb7155ad94b2515ea93
Nael Shiab <nael.shiab@gmail.com>
2025-05-02 09:21:51 -0400

    New options start and log for sleep function

--- src/other/sleep.ts
+++ src/other/sleep.ts
@@ -1,5 +1,7 @@
+import prettyDuration from "../format/prettyDuration.ts";
+
 /**
- * Pauses for a specified duration.
+ * Pauses execution for a specified duration.
  *
  * @example
  * Basic usage
@@ -7,8 +9,48 @@
  * await sleep(1000); // Pauses for 1 second
  * ```
  *
- * @param ms - The number of milliseconds to sleep.
+ * @example
+ * With options
+ * ```ts
+ * const start = new Date();
+ *
+ * // Perform some computation that takes time...
+ *
+ * await sleep(1000, { start }); // Pauses for 1 second minus the time taken for the computation
+ * ```
+ *
+ * @param ms - The number of milliseconds to pause.
+ * @param options - Optional parameters.
+ *   @param options.start - If you want to subtract the time taken to run something else, you can pass the start time here.
+ *   @param options.log - If true, it will log additional information.
  */
-export default function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+export default function sleep(
+  ms: number,
+  options: { start?: Date; log?: boolean } = {},
+): Promise<void> {
+  return new Promise((resolve) => {
+    if (options.start) {
+      const end = new Date();
+      const duration = end.getTime() - options.start.getTime();
+      const remaining = ms - duration;
+      if (remaining > 0) {
+        if (options.log) {
+          console.log(
+            `\nSleeping for ${prettyDuration(0, { end: remaining })}...`,
+          );
+        }
+        return setTimeout(resolve, remaining);
+      } else {
+        if (options.log) {
+          console.log(
+            `\nNo need to sleep, already took ${
+              prettyDuration(0, { end: duration })
+            }.`,
+          );
+        }
+        return resolve();
+      }
+    }
+    return setTimeout(resolve, ms);
+  });
 }
--- test/other/sleep.test.ts
+++ test/other/sleep.test.ts
@@ -9,3 +9,31 @@ Deno.test("should sleep for 100ms", async () => {
     true,
   );
 });
+Deno.test("should sleep for 100ms and log information", async () => {
+  await sleep(100, { log: true });
+
+  assertEquals(
+    true,
+    true,
+  );
+});
+Deno.test("should sleep for 50ms with the start option and log information", async () => {
+  const start = new Date();
+  await sleep(50);
+  await sleep(100, { start, log: true });
+
+  assertEquals(
+    true,
+    true,
+  );
+});
+Deno.test("should not sleep with the start option and log information", async () => {
+  const start = new Date();
+  await sleep(150);
+  await sleep(100, { start, log: true });
+
+  assertEquals(
+    true,
+    true,
+  );
+});
